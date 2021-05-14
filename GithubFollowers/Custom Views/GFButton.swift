0fae99a606d3e303fe2d93cb9bf57912bba690dc
Jagdeep singh <35945331+Jagdeepsingh8562@users.noreply.github.com>
2021-05-14 21:58:09 +0530

    coded custom Views(button , Textfield) for the app

---
+++ GithubFollowers/Custom Views/GFButton.swift
@@ -0,0 +1,37 @@
+//
+//  GFButton.swift
+//  GithubFollowers
+//
+//  Created by Jagdeep Singh on 14/05/21.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //custom code
+        configure()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    init(bgColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = bgColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
