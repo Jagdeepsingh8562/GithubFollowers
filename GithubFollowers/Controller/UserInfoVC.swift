727b17185c6b97b91beaa6b504c761cba1c9d5c3
Jagdeepsingh8562 <35945331+Jagdeepsingh8562@users.noreply.github.com>
2021-05-30 11:52:53 +0530

    added UserInfoHeaderVC in UserInfoVC

--- GithubFollowers/Controller/UserInfoVC.swift
+++ GithubFollowers/Controller/UserInfoVC.swift
@@ -9,19 +9,25 @@ import UIKit
 
 class UserInfoVC: UIViewController {
     
-    var username: String = ""
+    let headerView = UIView()
+    
+    var username: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self ,action: #selector(dismissVC) )
         navigationItem.rightBarButtonItem = doneButton
+        layoutUI()
         showLoadingView()
         NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
             guard let self = self else { return }
             self.dismissLoadingView()
             switch result {
             case .success(let user):
+                DispatchQueue.main.async {
+                    self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
+                }
                 print(user)
             case .failure(let errormessage):
                 self.presentGFAlertOnMainThread(title: "Error", message: errormessage.rawValue, buttonTitle: "Okay")
@@ -30,6 +36,29 @@ class UserInfoVC: UIViewController {
         }
        // print(username)
     }
+    
+    func layoutUI() {
+        view.addSubview(headerView)
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180)
+        ])
+    }
+    
+    func add(childVC: UIViewController, to contrainerView: UIView) {
+        addChild(childVC)
+        contrainerView.addSubview(childVC.view)
+        childVC.view.frame = contrainerView.bounds
+        childVC.didMove(toParent: self)
+    }
+    
     
     @objc func dismissVC(){
         DispatchQueue.main.async {
---
+++ GithubFollowers/Utillties/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GithubFollowers
+//
+//  Created by Jagdeep Singh on 30/05/21.
+//
+
+import Foundation
+
+enum SFSymbols  {
+    static let location = "mappin.and.ellipse"
+}
